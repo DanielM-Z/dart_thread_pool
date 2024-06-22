86c55c1a148c9760dec2d3103c18bfc2c59db193
PatrichHeadNToes <pomtier123@gmail.com>
2024-06-22 23:16:02 +0200

    Made a more stable approach that works with an overflowing threadpool

--- bin/dart_test_field.dart
+++ bin/dart_test_field.dart
@@ -2,22 +2,24 @@ import 'package:dart_test_field/dart_test_field.dart' as dart_test_field;
 import 'package:dart_test_field/dart_test_field.dart';
 
 void main(List<String> arguments) async {
-  print('Hello world: ${dart_test_field.calculate()}!');
+  //print('Hello world: ${dart_test_field.calculate()}!');
 
   ThreadPool tP = ThreadPool(numberThreads: 4);
   await tP.start();
 
+  Stopwatch stopwatch = Stopwatch()..start();
   List<int> li = await Future.wait<int>([
     dart_test_field.getItem(tP, 15),
-    dart_test_field.getItem(tP, 2),
-    dart_test_field.getItem(tP, 3),
-    dart_test_field.getItem(tP, 1),
+    dart_test_field.getItem(tP, 20),
+    dart_test_field.getItem(tP, 25),
+    dart_test_field.getItem(tP, 25),
   ]);
   print(li);
   List<int> li2 = await Future.wait<int>([
-    dart_test_field.getItem(tP, 1),
-    dart_test_field.getItem(tP, 2),
-    dart_test_field.getItem(tP, 3),
+    dart_test_field.getItem(tP, 100),
+    dart_test_field.getItem(tP, 200),
+    dart_test_field.getItem(tP, 300),
   ]);
   print(li2);
+  print('doSomething() executed in ${stopwatch.elapsed}');
 }
--- lib/dart_test_field.dart
+++ lib/dart_test_field.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:async';
 import 'dart:io';
 import 'dart:isolate';
@@ -17,35 +18,62 @@ Future<int> getItem(ThreadPool tP, int test) async {
 }
 
 int heavyComputation(int test) {
-  print("hi from heavy thread");
-  var total = 0.0;
-  for (var i = 0; i < 10000; i++) {
-    total += i;
-  }
-  print("finished work");
-  return -1 + test;
+  //print("hi from heavy thread");
+  sleep(Duration(seconds: 5));
+  //print("finished work");
+  return test;
 }
 
 void worker(SendPort sender) {
-  print("hi from init worker");
+  //print("hi from init worker");
   ReceivePort receiver = ReceivePort();
   sender.send(receiver.sendPort);
 
   receiver.listen((data) {
-    var callback = data as dynamic Function();
-    print("hi from new data worker");
-    sender.send(callback());
+    if (data is RequestData) {
+      //print("hi from new data worker");
+      ResponseData res = ResponseData(data: data.callback(), id: data.id);
+      sender.send(res);
+    } else {
+      print("Worker got wrong data type.");
+      throw Exception("Bad data passed to worker thread");
+    }
   });
+}
+
+class RequestData {
+  int id;
+  dynamic Function() callback;
+  RequestData({
+    required this.id,
+    required this.callback,
+  });
+}
+
+class ResponseData {
+  int id;
+  dynamic data;
+  ResponseData({
+    required this.id,
+    required this.data,
+  });
+
+  factory ResponseData.emptyRes() {
+    return ResponseData(id: -1, data: null);
+  }
 }
 
 class ThreadPool {
   int numberThreads;
   List<SendPort> allSenders = [];
-  // List<ReceivePort> allReceivers = [];
   List<Stream> allListeners = [];
   List<int> availablePorts = [];
+  final StreamController<bool> _portController = StreamController();
+  late Stream<bool> portTrigger;
 
-  ThreadPool({required this.numberThreads});
+  ThreadPool({required this.numberThreads}) {
+    portTrigger = _portController.stream.asBroadcastStream();
+  }
   Future<void> start() async {
     availablePorts = List.generate(numberThreads, (i) => i);
     List<ReceivePort> allReceivers = [];
@@ -61,7 +89,7 @@ class ThreadPool {
       var sendPort = await stream.first;
       if (sendPort is SendPort) {
         allSenders.add(sendPort);
-        print("sendport added");
+        //print("sendport added");
       } else {
         throw Exception("Setup error. Expected Sendport");
       }
@@ -83,28 +111,50 @@ class ThreadPool {
     return allListeners[index];
   }
 
-  /// Get available port and remove it from the available port list.
-  int getAvailablePort() {
-    if (allSenders.isEmpty) {
-      throw Exception("Sender was not setup. No sender available.");
-    }
+  /// Get available port and remove it from the available port list. If none are available wait
+  /// till one is available.
+  Future<int> getAvailablePort() async {
     if (availablePorts.isNotEmpty) {
       return availablePorts.removeLast();
-    } else {
-      throw Exception("All ports are used. Maybe you should use more threads");
     }
+
+    // Wait for a new port to be available and rerun the function.
+    await portTrigger.first;
+    return await getAvailablePort();
   }
 
   void addBackAvailablePort(int port) {
     availablePorts.add(port);
+    // Notify everyone that is waiting that a new thread is available
+    _portController.add(true);
   }
+
+  void close() {
+    _portController.close();
+  }
+
+  int counter = 0;
 
   Future<dynamic> executeFunctionInIsolate(
       dynamic Function() functionToCall) async {
-    int portToUse = getAvailablePort();
-    getSender(portToUse).send(functionToCall);
-    var res = await getListener(portToUse).first;
+    int portToUse = await getAvailablePort();
+    RequestData req = RequestData(id: counter++, callback: functionToCall);
+
+    // Send the request to the isolated thread.
+    getSender(portToUse).send(req);
+    ResponseData res = ResponseData.emptyRes();
+    // Check that the right id was passed. The id makes sure the right response is being served.
+    // This has to be done because the stream is a bit wacky and sometimes triggers the second call aswell.
+    // The stream listend to is a broadcasted stream.
+    while (res.id != req.id) {
+      var value = await getListener(portToUse).first;
+      if (value is! ResponseData) {
+        throw Exception("Bad response data passed.");
+      }
+      res = value;
+    }
+
     addBackAvailablePort(portToUse);
-    return res;
+    return res.data;
   }
 }
